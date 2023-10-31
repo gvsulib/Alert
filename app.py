f6f2c92d50aa9c396ea4fec5f921c13635f5e955
root <root@lib-support2.server.gvsu.edu>
2023-10-31 10:17:39 -0400

    adjusting CORS policy

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, Response, make_response
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app) 
+CORS(app, resources={r"/refresh/*": {"origins": ["https://www.gvsu.edu"]}}) 
 
 text="""
 // Set this to false if you don't want the alert to show. Set it to true to have it show up.
